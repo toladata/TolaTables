4c558836af7daf3831b042bcb1f6ad581c60cc03
Mahmood Khan <mkhan@mercycorps.org>
2016-03-28 16:07:42 -0700

    enabled token based auth for rest api

--- tola/settings/base.py
+++ tola/settings/base.py
@@ -220,6 +220,7 @@ DJANGO_APPS = (
 
 THIRD_PARTY_APPS = (
     'rest_framework',
+    'rest_framework.authtoken',
     'django_tables2',
     'crispy_forms',
     #'floppyforms',
@@ -339,6 +340,19 @@ LOGGING = {
 
 ########## END LOGGING CONFIGURATION
 
+########## REST CONFIGURATION
+# Add Pagination to Rest Framework lists
+REST_FRAMEWORK = {
+    'PAGINATE_BY': 10,
+    'DEFAULT_FILTER_BACKENDS': ('rest_framework.filters.DjangoFilterBackend',),
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.TokenAuthentication',
+    ),
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    )
+}
 
 ########## WSGI CONFIGURATION
 # See: https://docs.djangoproject.com/en/dev/ref/settings/#wsgi-application
