45d072a2b404245d210d9353a715a6841a38ee55
Mahmood Khan <mkhan@mercycorps.org>
2016-03-28 16:00:07 -0700

    added missing import

--- silo/models.py
+++ silo/models.py
@@ -9,7 +9,7 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 from datetime import datetime
 from django.conf import settings
-
+from rest_framework.authtoken.models import Token
 
 #New user created generate a token
 @receiver(post_save, sender=settings.AUTH_USER_MODEL)
