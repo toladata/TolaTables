4a6715226fa38aa1aa97ce5501f0076762c631ce
Mahmood Khan <mkhan@mercycorps.org>
2016-04-07 16:48:58 -0700

    removed deprecated TEMPLATE_DEBUG and commented out code

--- tola/settings/base.py
+++ tola/settings/base.py
@@ -24,8 +24,6 @@ path.append(DJANGO_ROOT)
 # See: https://docs.djangoproject.com/en/dev/ref/settings/#debug
 DEBUG = False
 
-# See: https://docs.djangoproject.com/en/dev/ref/settings/#template-debug
-TEMPLATE_DEBUG = DEBUG
 ########## END DEBUG CONFIGURATION
 
 
@@ -181,6 +179,7 @@ TEMPLATES = [
                 'django.core.context_processors.request',
                 'tola.context_processors.get_silos',
             ],
+            'DEBUG': DEBUG,
             'builtins': [
                 'django.contrib.staticfiles.templatetags.staticfiles',
                 'silo.templatetags.underscoretags',
@@ -273,35 +272,6 @@ SOCIAL_AUTH_LOGIN_REDIRECT_URL = '/'
 
 ########## LOGGING CONFIGURATION
 # See: https://docs.djangoproject.com/en/dev/ref/settings/#logging
-# A sample logging configuration. The only tangible logging
-# performed by this configuration is to send an email to
-# the site admins on every HTTP 500 error when DEBUG=False.
-# See http://docs.djangoproject.com/en/dev/topics/logging for
-# more details on how to customize your logging configuration.
-#LOGGING = {
-#    'version': 1,
-#    'disable_existing_loggers': False,
-#    'filters': {
-#        'require_debug_false': {
-#            '()': 'django.utils.log.RequireDebugFalse'
-#        }
-#    },
-#    'handlers': {
-#        'mail_admins': {
-#            'level': 'ERROR',
-#            'filters': ['require_debug_false'],
-#            'class': 'django.utils.log.AdminEmailHandler'
-#        }
-#    },
-#    'loggers': {
-#        'django.request': {
-#            'handlers': ['mail_admins'],
-#            'level': 'ERROR',
-#            'propagate': True,
-#        },
-#    }
-#}
-
 
 import os
 PROJECT_PATH = dirname(dirname(dirname(abspath(__file__))))
