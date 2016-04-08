3d4173d0150717e5cf89067127a00711b99ff84d
Mahmood Khan <mkhan@mercycorps.org>
2016-04-08 09:07:46 -0700

    #63 resolved autopush to gsheet

--- silo/management/commands/push_to_gsheet.py
+++ silo/management/commands/push_to_gsheet.py
@@ -3,10 +3,10 @@ from requests.auth import HTTPDigestAuth
 
 from django.core.management.base import BaseCommand, CommandError
 from django.db.models import Q
+from operator import and_, or_
 from django.utils import timezone
-from django.contrib.auth.models import User
 
-from silo.models import *
+from silo.models import Silo, Read, ReadType
 from tola.util import siloToDict, combineColumns
 from silo.google_views import *
 
@@ -27,7 +27,9 @@ class Command(BaseCommand):
 
         # Get all silos that have a unique field setup, have autopush frequency selected and autopush frequency is the same as specified in this command line argument.
         silos = Silo.objects.filter(unique_fields__isnull=False, reads__autopush_frequency__isnull=False, reads__autopush_frequency = frequency).distinct()
-        read_types = ReadType.objects.filter(Q(read_type="GSheet Import")|Q(read_type='Google Spreadsheet'))
+        readtypes = ReadType.objects.filter( Q(read_type__iexact="GSheet Import") | Q(read_type__iexact='Google Spreadsheet') )
+        #readtypes = ReadType.objects.filter(reduce(or_, [Q(read_type__iexact="GSheet Import"), Q(read_type__iexact='Google Spreadsheet')] ))
+
         for silo in silos:
             reads = silo.reads.filter(reduce(or_, [Q(type=read.id) for read in readtypes])).filter(autopush_frequency__isnull=False, autopush_frequency = frequency)
             for read in reads:
@@ -39,9 +41,12 @@ class Command(BaseCommand):
                 if credential is None or credential.invalid == True:
                     self.stdout.write("There was a Google credential problem with user: %s for gsheet %s" % (silo.owner, read.pk))
                     continue
-
-                suc = export_to_google_spreadsheet(credential_json, silo, read.resource_id)
+                #self.stdout.write(read.resource_id)
+                #self.stdout.write(json.dumps(credential_json))
+                suc = export_to_google_spreadsheet(credential_json, silo.pk, read.resource_id)
+                #suc = True
                 if suc == False:
                     self.stdout.write("The Google sheet export failed for user: %s  with ghseet: %s" % (silo.owner, read.pk))
                 self.stdout.write('Successfully pushed the READ_ID, "%s", to Gsheet for %s' % (read.pk, silo.owner))
+
         self.stdout.write("done executing gsheet export command job")
