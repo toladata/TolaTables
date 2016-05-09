50628550cea4b96a65f45098ba5a16dcd0eaf2c4
Mahmood Khan <mkhan@mercycorps.org>
2016-05-09 15:08:58 -0700

    to make sure source is always saved

--- silo/views.py
+++ silo/views.py
@@ -376,8 +376,8 @@ def showRead(request, id):
     if request.method == 'POST':
         form = ReadForm(request.POST, request.FILES, instance=read_instance)
         if form.is_valid():
+            read = form.save()
             if form.instance.type.read_type == "CSV":
-                read = form.save()
                 return HttpResponseRedirect("/file/" + str(read.id) + "/")
 
             if form.instance.autopull_frequency or form.instance.autopush_frequency:
