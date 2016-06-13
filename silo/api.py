a39c6ad1d10374d85172b33f3bd0b9ff6d19abf6
Mahmood Khan <mkhan@mercycorps.org>
2016-06-13 15:08:27 -0700

    paginate by 200 the /silos api endpiont if it is the superuser requesting it

--- silo/api.py
+++ silo/api.py
@@ -10,6 +10,8 @@ from .serializers import *
 from silo.permissions import IsOwnerOrReadOnly
 from django.contrib.auth.models import User
 from rest_framework.decorators import detail_route, list_route
+from rest_framework import pagination
+
 
 import django_filters
 
@@ -43,6 +45,7 @@ class SiloViewSet(viewsets.ModelViewSet):
     def get_queryset(self):
         user = self.request.user
         if user.is_superuser:
+            pagination.PageNumberPagination.page_size = 200
             return Silo.objects.all()
         return Silo.objects.filter(owner=user)
 
