bba394ea5940131c467df325ddd0af8963a25e30
Greg Lind <gwlind@gmail.com>
2016-11-16 09:23:10 +0700

    remove broken swagger api

--- tola/urls.py
+++ tola/urls.py
@@ -33,7 +33,6 @@ router.register(r'tag', TagViewSet)
 urlpatterns =[
     url(r'^api/', include(router.urls)),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-    url(r'^api/docs/', schema_view),
 
     url(r'^admin/', include(admin.site.urls)),
     url(r'^$', views.index, name='index'),
--- tola/views.py
+++ tola/views.py
@@ -95,14 +95,3 @@ def logout_view(request):
     # Redirect to a success page.
     return HttpResponseRedirect("/")
 
-
-from rest_framework.decorators import api_view, renderer_classes
-from rest_framework import response, schemas
-from rest_framework_swagger.renderers import OpenAPIRenderer, SwaggerUIRenderer
-
-
-@api_view()
-@renderer_classes([OpenAPIRenderer, SwaggerUIRenderer])
-def schema_view(request):
-    generator = schemas.SchemaGenerator(title='Bookings API')
-    return response.Response(generator.get_schema(request=request))
