993441fcac85e0c372f73156fcf83f8d74a4eb88
Mahmood Khan <mkhan@mercycorps.org>
2017-01-18 08:52:37 -0800

    adding board models to the admin interface

--- board/admin.py
+++ board/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import *
+
+admin.site.register(Board)
+admin.site.register(Graph)
+admin.site.register(GraphModel)
+admin.site.register(Item)
+admin.site.register(GraphInput)
