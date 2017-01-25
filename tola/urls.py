4358ae735d7dc4b9b3d1f7cfb98119f9688e10c8
Mahmood Khan <mkhan@mercycorps.org>
2017-01-25 13:17:41 -0800

    getting rid of the underscored

--- tola/urls.py
+++ tola/urls.py
@@ -24,7 +24,7 @@ admin.autodiscover()
 #REST FRAMEWORK
 router = routers.DefaultRouter(trailing_slash=False)
 router.register(r'silo', SiloViewSet, base_name="silo")
-router.register(r'user_silos', SilosByUser, base_name='user_silos')
+router.register(r'usersilos', SilosByUser, base_name='usersilos')
 router.register(r'public_tables', PublicSiloViewSet, base_name="public_tables")
 router.register(r'users', UserViewSet)
 router.register(r'read', ReadViewSet)
