3bfa8504376b599c2e89ab9165eba628f3b27d62
Mahmood Khan <mkhan@mercycorps.org>
2017-01-25 10:20:39 -0800

    disabling trailing slashes on api endpoints to make Ember TolaBoard happy

--- tola/urls.py
+++ tola/urls.py
@@ -22,7 +22,7 @@ from django.contrib import admin
 admin.autodiscover()
 
 #REST FRAMEWORK
-router = routers.DefaultRouter()
+router = routers.DefaultRouter(trailing_slash=False)
 router.register(r'silo', SiloViewSet, base_name="silo")
 router.register(r'user_silos', SilosByUser, base_name='user_silos')
 router.register(r'public_tables', PublicSiloViewSet, base_name="public_tables")
