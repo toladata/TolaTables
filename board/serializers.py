fc9519a2d7d44b77243c6a8a7cfbf5b25450fd6e
Mahmood Khan <mkhan@mercycorps.org>
2017-02-07 09:36:28 -0800

    added boardsilos api endpoint for tolaboard

--- board/serializers.py
+++ board/serializers.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers
 from .models import Board, Graph, GraphModel, Item, GraphInput
+from silo.models import Silo
 
 class BoardSerializer(serializers.ModelSerializer):
 
@@ -36,3 +37,15 @@ class GraphInputSerializer(serializers.ModelSerializer):
     class Meta:
         model = GraphInput
         exclude = ('created', 'updated')
+
+
+class SiloSerializer(serializers.ModelSerializer):
+    data = serializers.SerializerMethodField()
+    class Meta:
+        model = Silo
+        fields = ('owner', 'name', 'reads', 'description', 'create_date', 'id', 'data','shared','tags','public')
+        depth =1
+
+    def get_data(self, obj):
+        link = "/api/silo/" + str(obj.id) + "/data"
+        return (self.context['request'].build_absolute_uri(link))
