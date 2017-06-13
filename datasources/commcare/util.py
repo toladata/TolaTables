75285e6b2bd2bcbe74c6dbc032e9ceb4a309b6d6
Mercy Corps <alexdgarten@gmail.com>
2017-06-13 09:59:29 -0700

    Made comments in commcare util.py clearer

--- datasources/commcare/util.py
+++ datasources/commcare/util.py
@@ -10,10 +10,12 @@ def getProjects():
         projects.append(read.read_url.split('/')[4])
     return list(set(projects))
 
-#since the commcare dictionary does not use the proper column names and instead uses column
-#idnetifiers this funciton changes the dictionary to use the proper column names
-#used in commcoare/views.py for saveCommCareData
 def useHeaderName(columns, data):
+    """
+    since the commcare dictionary does not use the proper column names and instead uses column
+    idnetifiers this funciton changes the dictionary to use the proper column names
+    used in commcoare/views.py for saveCommCareData
+    """
     for row in data:
         for column in columns:
             row[column['header']] = row.pop(column['slug'])
