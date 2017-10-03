caabfbaa25c186f566e522362667712ba4f7c862
Greg Lind <gwlind@gmail.com>
2017-10-03 10:08:16 +0200

    update shared user

--- silo/api.py
+++ silo/api.py
@@ -148,7 +148,7 @@ class SiloViewSet(viewsets.ReadOnlyModelViewSet):
             #pagination.PageNumberPagination.page_size = 200
             return Silo.objects.all()
 
-        return Silo.objects.filter(Q(owner__user=user) | Q(public=True) | Q(shared=self.request.user))
+        return Silo.objects.filter(Q(owner__user=user) | Q(public=True) | Q(shared__user=self.request.user))
 
 
     @detail_route()
