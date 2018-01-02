659bdb2fa3f72171c97954e63e3df6ecc4c89514
Greg Lind <gwlind@gmail.com>
2018-01-02 13:52:39 +0100

    update oauth2callback url for picker

--- tola/settings/local.py
+++ tola/settings/local.py
@@ -62,7 +62,10 @@ if os.getenv('TOLA_HOSTNAME') is not None:
 USE_X_FORWARDED_HOST = True if os.getenv('TOLA_USE_X_FORWARDED_HOST') == 'True' else False
 
 ########## GOOGLE CLIENT CONFIG ###########
-GOOGLE_REDIRECT_URL = os.getenv('TABLES_URL') + '/oauth2callback/'
+try:
+    GOOGLE_REDIRECT_URL = os.getenv('TABLES_URL') + '/oauth2callback/'
+except KeyError:
+    GOOGLE_REDIRECT_URL = 'http://localhost:8000/oauth2callback/'
 #GOOGLE_STEP2_URI = 'http://tola.mercycorps.org/gwelcome'
 #GOOGLE_CLIENT_ID = 'xxxxxxx.apps.googleusercontent.com'
 #GOOGLE_CLIENT_SECRET = 'xxxxxxxxx'
