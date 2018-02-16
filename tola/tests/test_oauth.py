7e521a59e7c1606337de81c72bbc85f59fcbacf3
Jeferson Moura <jefmmoreira@gmail.com>
2018-02-16 13:09:32 +0100

    Update user_to_tola() to create data for org instead of using from the response

--- tola/tests/test_oauth.py
+++ tola/tests/test_oauth.py
@@ -56,12 +56,7 @@ class OAuthTest(TestCase):
             },
             'organization': {
                 'name': self.org.name,
-                'url': '',
-                'industry': '',
-                'sector': '',
                 'organization_uuid': self.org.organization_uuid,
-                'chargebee_subscription_id': '',
-                'chargebee_used_seats': ''
             }
         }
 
@@ -98,6 +93,33 @@ class OAuthTest(TestCase):
         tola_user = TolaUser.objects.get(user=user)
 
         self.assertEqual(tola_user.organization.name, new_org.name)
+
+    def test_user_to_tola_org_extra_fields(self):
+        response = {
+            'tola_user': {
+                'tola_user_uuid': '13dac835-3860-4d9d-807e-d36a3c106057',
+                'name': 'John Lennon',
+                'employee_number': None,
+                'title': 'mr',
+                'privacy_disclaimer_accepted': True,
+            },
+            'organization': {
+                'name': self.org.name,
+                'url': '',
+                'industry': '',
+                'sector': '',
+                'organization_uuid': self.org.organization_uuid,
+                'chargebee_subscription_id': '',
+                'chargebee_used_seats': ''
+            }
+        }
+
+        user = factories.User(first_name='John', last_name='Lennon')
+        auth_pipeline.user_to_tola(None, user, response)
+        tola_user = TolaUser.objects.get(name='John Lennon')
+
+        self.assertEqual(tola_user.name, response['tola_user']['name'])
+        self.assertEqual(tola_user.organization.name, self.org.name)
 
     def test_get_or_create_user_without_user(self):
         """
