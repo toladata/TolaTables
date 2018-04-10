6d48d75a5eb1f7ca65b815394575151a847fec05
leon <git@leonholub.com>
2018-04-10 17:14:54 +0200

    Added Tests for export_to_gsheet view

--- silo/tests/test_gviews_v4.py
+++ silo/tests/test_gviews_v4.py
@@ -4,7 +4,7 @@ import os
 import urllib
 
 from django.contrib.auth.models import User
-from django.core.exceptions import ImproperlyConfigured
+from django.core.exceptions import ImproperlyConfigured, ObjectDoesNotExist
 from django.test import TestCase, override_settings
 from django.urls import reverse
 from oauth2client.client import AccessTokenCredentialsError
@@ -14,6 +14,8 @@ from mock import Mock, patch
 
 import silo.gviews_v4 as gviews_v4
 import factories
+from silo.models import LabelValueStore
+from tola.util import saveDataToSilo
 
 
 class GetOauthFlowTest(TestCase):
@@ -56,6 +58,193 @@ class GetOauthFlowTest(TestCase):
         self.assertEqual(flow.client_secret, 'pass')
         self.assertEqual(flow.scope, gviews_v4.SCOPE)
         self.assertEqual(flow.redirect_uri, 'url')
+
+
+class ExportToGSheetTest(TestCase):
+
+    def _import_json(self, silo, read):
+        filename = os.path.join(os.path.dirname(__file__),
+                                'sample_data/moviesbyearnings2013.json')
+        with open(filename, 'r') as f:
+            data = json.load(f)
+            saveDataToSilo(silo, data, read)
+
+    def setUp(self):
+        self.org = factories.Organization()
+        self.tola_user = factories.TolaUser(organization=self.org)
+
+        self.read = factories.Read(read_name="test_data",
+                                   owner=self.tola_user.user)
+        self.silo = factories.Silo(owner=self.tola_user.user,
+                                   reads=[self.read])
+        self._import_json(self.silo, self.read)
+
+        self.factory = APIRequestFactory()
+
+    def tearDown(self):
+        # Have to remove the created lvs
+        lvss = LabelValueStore.objects.filter(silo_id=self.silo.id)
+        for lvs in lvss:
+            lvs.delete()
+
+    @patch('silo.gviews_v4.export_to_gsheet_helper')
+    def test_export_to_gsheet(self, mock_gsheet_helper):
+        spreadsheet_id = None
+        query = {}
+        cols = []
+
+        mock_gsheet_helper.return_value = []
+
+        url = reverse('export_new_gsheet', kwargs={'id': self.silo.pk}) + \
+              '?&query='+str(query)+'&shown_cols='+str(cols)
+        request = self.factory.get(url, follow=True)
+        request.user = self.tola_user.user
+        response = gviews_v4.export_to_gsheet(request, self.silo.pk)
+
+        mock_gsheet_helper.assert_called_once_with(self.tola_user.user,
+                                                   spreadsheet_id,
+                                                   self.silo.pk, query,
+                                                   cols)
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response.url, reverse('listSilos'))
+
+    @patch('silo.gviews_v4.export_to_gsheet_helper')
+    def test_export_to_gsheet_no_params(self, mock_gsheet_helper):
+        spreadsheet_id = None
+        query = {}
+        expected_cols = ['cnt', 'grs', 'tit', 'rank', 'opn', 'yr']
+
+        mock_gsheet_helper.return_value = []
+
+        url = reverse('export_new_gsheet', kwargs={'id': self.silo.pk})
+        request = self.factory.get(url, follow=True)
+        request.user = self.tola_user.user
+        response = gviews_v4.export_to_gsheet(request, self.silo.pk)
+
+        mock_gsheet_helper.assert_called_once_with(self.tola_user.user,
+                                                   spreadsheet_id,
+                                                   self.silo.pk,
+                                                   query,
+                                                   expected_cols)
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response.url, reverse('listSilos'))
+
+    @patch('silo.gviews_v4.export_to_gsheet_helper')
+    def test_export_to_gsheet_wrong_column_type(self, mock_gsheet_helper):
+        query = {}
+
+        mock_gsheet_helper.return_value = []
+
+        url = reverse('export_new_gsheet', kwargs={'id': self.silo.pk}) + \
+              '?&query='+str(query)+'&shown_cols={"yr", "rank", "opn"}'
+
+        with self.assertRaises(ValueError):
+            request = self.factory.get(url, follow=True)
+            request.user = self.tola_user.user
+            gviews_v4.export_to_gsheet(request, self.silo.pk)
+
+        mock_gsheet_helper.assert_not_called()
+
+    @patch('silo.gviews_v4.export_to_gsheet_helper')
+    def test_export_to_gsheet_wrong_query_type(self, mock_gsheet_helper):
+        query = ["test"]
+
+        mock_gsheet_helper.return_value = []
+
+        url = reverse('export_new_gsheet', kwargs={'id': self.silo.pk}) + \
+              '?&query='+str(query)+'&shown_cols=["yr", "rank", "opn"]'
+
+        with self.assertRaises(ValueError):
+            request = self.factory.get(url, follow=True)
+            request.user = self.tola_user.user
+            gviews_v4.export_to_gsheet(request, self.silo.pk)
+
+        mock_gsheet_helper.assert_not_called()
+
+    @patch('silo.gviews_v4.export_to_gsheet_helper')
+    def test_export_to_gsheet_no_silo(self, mock_gsheet_helper):
+        mock_gsheet_helper.return_value = []
+
+        url = reverse('export_new_gsheet', kwargs={'id': 0})
+
+        with self.assertRaises(ObjectDoesNotExist):
+            request = self.factory.get(url, follow=True)
+            request.user = self.tola_user.user
+            gviews_v4.export_to_gsheet(request, 0)
+
+        mock_gsheet_helper.assert_not_called()
+
+    @patch('silo.gviews_v4.export_to_gsheet_helper')
+    def test_export_to_gsheet_with_cols(self, mock_gsheet_helper):
+        spreadsheet_id = None
+        query = {}
+        cols = ["yr", "rank", "opn"]
+        mock_gsheet_helper.return_value = []
+
+        url = reverse('export_new_gsheet', kwargs={'id': self.silo.pk}) + \
+              '?&query='+str(query)+'&shown_cols=["yr", "rank", "opn"]'
+
+        request = self.factory.get(url, follow=True)
+        request.user = self.tola_user.user
+        response = gviews_v4.export_to_gsheet(request, self.silo.pk)
+
+        mock_gsheet_helper.assert_called_once_with(self.tola_user.user,
+                                                   spreadsheet_id,
+                                                   self.silo.pk,
+                                                   query,
+                                                   cols)
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response.url, reverse('listSilos'))
+
+    @patch('silo.gviews_v4.export_to_gsheet_helper')
+    def test_export_to_gsheet_with_query(self, mock_gsheet_helper):
+        query = {"$or": [{"First_Name": {"$nin": ["1", 1.0, 1]}}]}
+        expected_cols = []
+        mock_gsheet_helper.return_value = []
+
+        url = reverse('export_new_gsheet', kwargs={'id': self.silo.pk}) + \
+              '?&query={"$or": [{"First_Name": {"$nin": ["1", 1.0, 1]}}]}' \
+              '&shown_cols=[]'
+
+        request = self.factory.get(url, follow=True)
+        request.user = self.tola_user.user
+        response = gviews_v4.export_to_gsheet(request, self.silo.pk)
+
+        mock_gsheet_helper.assert_called_once_with(self.tola_user.user,
+                                                   None,
+                                                   self.silo.pk,
+                                                   query,
+                                                   expected_cols)
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response.url, reverse('listSilos'))
+
+    @patch('silo.gviews_v4.export_to_gsheet_helper')
+    def test_export_to_gsheet_redirect_uri(self, mock_gsheet_helper):
+        spreadsheet_id = None
+        query = {}
+        expected_cols = ['cnt', 'grs', 'tit', 'rank', 'opn', 'yr']
+
+        url = reverse('export_new_gsheet', kwargs={'id': self.silo.pk})
+        request = self.factory.get(url, follow=True)
+        setattr(request, 'session', {})
+
+        mock_gsheet_helper.return_value = [{
+            "level": 123,
+            "msg": "Requires Google Authorization Setup",
+            "redirect": "redirect_url",
+            "redirect_uri_after_step2": True
+        }]
+
+        request.user = self.tola_user.user
+        response = gviews_v4.export_to_gsheet(request, self.silo.pk)
+
+        mock_gsheet_helper.assert_called_once_with(self.tola_user.user,
+                                                   spreadsheet_id,
+                                                   self.silo.pk,
+                                                   query,
+                                                   expected_cols)
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response.url, "redirect_url")
 
 
 class GetSheetsFromGoogleTest(TestCase):
